4023ceebea0ec0e484b20a3a233cb979032c48f1
Shaheen Parvizi <sparvizi@volleyboast.com>
2018-04-26 00:21:05 -0500

    New Changes

---
+++ WebApplication1/WebApplication1/Controllers/FormController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult FormOne(string Address1, string Address2, string City, string State, string ZipCode)
+        {
+            return View("NewPage");
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/FormData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class FormData
+    {
+        public string Address1 { get; set; }
+
+        public string Address2 { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string ZipCode { get; set; }
+
+ 
+    }
+}
